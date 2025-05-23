2f2c6606f559af2d41d7ba9ca17b39bcd42fe237
Debaug <thibault@itrature.net>
2025-05-23 22:27:21 +0200

    MVP

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,13 @@ pub fn run() -> Result<()> {
     )
     .add_sub_app(LogSubApp)
     .add_sub_app(DanceSubAppBuilder {
-        n_points: 1_000_000,
+        n_points: 2_000_000,
         transformation_colors: vec![
             vec4(0.9, 0.9, 0.6, 1.0),
             vec4(0.6, 0.9, 0.9, 1.0),
             vec4(0.9, 0.6, 0.9, 1.0),
             vec4(0.9, 0.6, 0.4, 1.0),
+            vec4(0.4, 0.6, 0.9, 1.0),
         ],
     })
     .run()
